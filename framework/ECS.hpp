af788b4c9c0570c4818ef251d4e0d470bebdfea2
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-02 16:46:12 -0300

    Refactored the ECS container to make it easier to add components

--- framework/ECS.hpp
+++ framework/ECS.hpp
@@ -15,90 +15,38 @@ namespace ecs {
     template<typename T>
     using ComponentData = std::unordered_map<Entity, T>;
 
-    struct ECS {
-        Entity nextEntityId = 0;
-        ComponentData<Bounds> bounds;
-        ComponentData<CircularObject> circularObjects;
-        ComponentData<Drawable> drawables;
-        ComponentData<RectangularObject> rectangularObjects;
-        ComponentData<ScoreListener> scoreListeners;
-        ComponentData<ScoringBounds> scoringBounds;
-        ComponentData<Velocity> velocities;
-    };
+    namespace __detail {
+        template<typename T>
+        struct FieldContainer {
+            ComponentData<T> field;
+            static constexpr ComponentData<T> FieldContainer::* address = &FieldContainer::field;
+        };
+
+        template<typename... Ts>
+        struct GenericECS : FieldContainer<Ts>... {
+            Entity nextEntityId = 0;
+        };
+    }
+
+    using ECS = __detail::GenericECS<
+        Bounds,
+        CircularObject,
+        Drawable,
+        RectangularObject,
+        ScoreListener,
+        ScoringBounds,
+        Velocity
+    >;
 
     template<typename T>
-    constexpr ComponentData<T>& entityData(ECS&);
+    constexpr ComponentData<T>& entityData(ECS& ecs) {
+        constexpr auto field = __detail::FieldContainer<T>::address;
+        return ecs.*field;
+    }
 
     template<typename T>
-    constexpr const ComponentData<T>& entityData(const ECS&);
-
-    template<>
-    constexpr ComponentData<Bounds>& entityData(ECS& ecs) {
-        return ecs.bounds;
-    }
-
-    template<>
-    constexpr const ComponentData<Bounds>& entityData(const ECS& ecs) {
-        return ecs.bounds;
-    }
-
-    template<>
-    constexpr ComponentData<CircularObject>& entityData(ECS& ecs) {
-        return ecs.circularObjects;
-    }
-
-    template<>
-    constexpr const ComponentData<CircularObject>& entityData(const ECS& ecs) {
-        return ecs.circularObjects;
-    }
-
-    template<>
-    constexpr ComponentData<Drawable>& entityData(ECS& ecs) {
-        return ecs.drawables;
-    }
-
-    template<>
-    constexpr const ComponentData<Drawable>& entityData(const ECS& ecs) {
-        return ecs.drawables;
-    }
-
-    template<>
-    constexpr ComponentData<RectangularObject>& entityData(ECS& ecs) {
-        return ecs.rectangularObjects;
-    }
-
-    template<>
-    constexpr const ComponentData<RectangularObject>& entityData(const ECS& ecs) {
-        return ecs.rectangularObjects;
-    }
-
-    template<>
-    constexpr ComponentData<ScoreListener>& entityData(ECS& ecs) {
-        return ecs.scoreListeners;
-    }
-
-    template<>
-    constexpr const ComponentData<ScoreListener>& entityData(const ECS& ecs) {
-        return ecs.scoreListeners;
-    }
-
-    template<>
-    constexpr ComponentData<ScoringBounds>& entityData(ECS& ecs) {
-        return ecs.scoringBounds;
-    }
-
-    template<>
-    constexpr const ComponentData<ScoringBounds>& entityData(const ECS& ecs) {
-        return ecs.scoringBounds;
-    }
-
-    template<>
-    constexpr ComponentData<Velocity>& entityData(ECS& ecs) {
-        return ecs.velocities;
-    }
-
-    template<>
-    constexpr const ComponentData<Velocity>& entityData(const ECS& ecs) {
-        return ecs.velocities;
+    constexpr const ComponentData<T>& entityData(const ECS& ecs) {
+        constexpr auto field = __detail::FieldContainer<T>::address;
+        return ecs.*field;
     }
 }
